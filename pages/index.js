1bd91a17156daccbd3b8cfc928eabaec4703ee7d
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2023-02-28 22:20:34 +0000

    working slack install button

--- pages/index.js
+++ pages/index.js
@@ -229,12 +229,11 @@ export default function Home({ chatId }) {
           }} style={{ border: "2px solid green", padding: "2%", borderRadius: "10px" }}>Schedule Demo</a>
           <a href="https://berri.ai/" target="_blank" style={{ padding: "2%", backgroundColor: "#048c2c", borderRadius: "10px" }}>+ New App</a>
           <div>
+            <a href="https://tempslack.ishaan-jaff.repl.co/slack/install">
+              <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+            </a>
 
-            <Link href={'/new-page?api_endpoint=' + api_endpoint} passHref>
-              <div>
-                <button><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></button>
-              </div>
-            </Link>
+
           </div>
 
 
