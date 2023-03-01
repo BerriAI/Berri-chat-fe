53ee2ba03e092ef72804f5a866829673895acee2
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2023-03-01 04:55:11 +0000

    with new tab on clickng button

--- pages/index.js
+++ pages/index.js
@@ -229,9 +229,7 @@ export default function Home({ chatId }) {
           }} style={{ border: "2px solid green", padding: "2%", borderRadius: "10px" }}>Schedule Demo</a>
           <a href="https://berri.ai/" target="_blank" style={{ padding: "2%", backgroundColor: "#048c2c", borderRadius: "10px" }}>+ New App</a>
           <div>
-            <a href="https://tempslack.ishaan-jaff.repl.co/slack/install">
-              <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
-            </a>
+            <a href="https://tempslack.ishaan-jaff.repl.co/slack/install" target="_blank"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
 
 
           </div>
